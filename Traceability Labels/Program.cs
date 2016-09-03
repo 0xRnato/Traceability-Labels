b9f9aab31cfdf21bb9761577d1f60a0ddaa2297b
Renato Neto <Renato Neto>
2016-09-03 12:37:42 -0300

    First commit

---
+++ Traceability Labels/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+
+namespace Traceability_Labels
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
