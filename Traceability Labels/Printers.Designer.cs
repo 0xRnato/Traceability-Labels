bb3a5722fb4d2243634987150a0b472da50bc911
Renato Neto <Renato Neto>
2016-09-05 01:25:42 -0300

    Printer select dialog

---
+++ Traceability Labels/Printers.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace Traceability_Labels
+{
+    partial class Printers
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.cbox_Printers = new System.Windows.Forms.ComboBox();
+            this.btn_Save = new System.Windows.Forms.Button();
+            this.btn_Cancel = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(31, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(158, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "IMPRESSORAS INSTALADAS:";
+            // 
+            // cbox_Printers
+            // 
+            this.cbox_Printers.FormattingEnabled = true;
+            this.cbox_Printers.Location = new System.Drawing.Point(195, 12);
+            this.cbox_Printers.Name = "cbox_Printers";
+            this.cbox_Printers.Size = new System.Drawing.Size(275, 21);
+            this.cbox_Printers.TabIndex = 1;
+            // 
+            // btn_Save
+            // 
+            this.btn_Save.Location = new System.Drawing.Point(314, 39);
+            this.btn_Save.Name = "btn_Save";
+            this.btn_Save.Size = new System.Drawing.Size(75, 23);
+            this.btn_Save.TabIndex = 2;
+            this.btn_Save.Text = "Salvar";
+            this.btn_Save.UseVisualStyleBackColor = true;
+            this.btn_Save.Click += new System.EventHandler(this.btn_Save_Click);
+            // 
+            // btn_Cancel
+            // 
+            this.btn_Cancel.Location = new System.Drawing.Point(395, 39);
+            this.btn_Cancel.Name = "btn_Cancel";
+            this.btn_Cancel.Size = new System.Drawing.Size(75, 23);
+            this.btn_Cancel.TabIndex = 3;
+            this.btn_Cancel.Text = "Cancelar";
+            this.btn_Cancel.UseVisualStyleBackColor = true;
+            this.btn_Cancel.Click += new System.EventHandler(this.btn_Cancel_Click);
+            // 
+            // Printers
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(504, 74);
+            this.Controls.Add(this.btn_Cancel);
+            this.Controls.Add(this.btn_Save);
+            this.Controls.Add(this.cbox_Printers);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Printers";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Impressoras";
+            this.Load += new System.EventHandler(this.Printers_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox cbox_Printers;
+        private System.Windows.Forms.Button btn_Save;
+        private System.Windows.Forms.Button btn_Cancel;
+    }
+}
---
+++ Traceability Labels/Printers.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using System.Drawing.Printing;
+
+namespace Traceability_Labels
+{
+    public partial class Printers : Form
+    {
+        public Printers()
+        {
+            InitializeComponent();
+        }
+
+        private void Printers_Load(object sender, EventArgs e)
+        {
+            foreach (var printer in PrinterSettings.InstalledPrinters)
+            {
+                var item = printer;
+                cbox_Printers.Items.Add(item);
+            }
+        }
+
+        private void btn_Cancel_Click(object sender, EventArgs e)
+        {
+            Hide();
+        }
+
+        private void btn_Save_Click(object sender, EventArgs e)
+        {
+            Global.printerName = cbox_Printers.SelectedItem.ToString();
+            MessageBox.Show("Impressora selecionada com sucesso!");
+            Hide();
+        }
+    }
+}
